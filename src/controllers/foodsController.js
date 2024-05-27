6a2f21ef9a808947e165eb47c4ed0fcfa9c33353
fadvr <fadnoor31@gmail.com>
2024-05-27 23:13:29 +0700

    add foodGetByName func

--- src/controllers/foodsController.js
+++ src/controllers/foodsController.js
@@ -45,3 +45,39 @@ const addFood = async (request, h) => {
     message: "Food added successfully",
   };
 };
+
+const foodGetByName = async (request, h) => {
+  const { name } = request.params;
+
+  if (!name) {
+    const response = h.response({
+      status: "fail",
+      message: "Name is required",
+    });
+    response.code(400);
+    return response;
+  }
+
+  const foodSnapshot = await firestore
+    .collection("foods")
+    .where("name", "=", name)
+    .get();
+
+  if (foodSnapshot.empty) {
+    const response = h.response({
+      status: "fail",
+      message: "Food not found",
+    });
+    response.code(404);
+    return response;
+  }
+
+  const food = foodSnapshot.docs[0].data();
+
+  return {
+    status: "success",
+    data: food,
+  };
+};
+
+module.exports = { addFood, foodGetByName };
