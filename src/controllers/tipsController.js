cac82c27bf765c47c88a24f7b3e7cf7292519e17
vrhidayat <fadli.hidayat.nur@gmail.com>
2024-06-11 15:20:25 +0700

    fix getAllTips functions

--- src/controllers/tipsController.js
+++ src/controllers/tipsController.js
@@ -34,11 +34,24 @@ const tipsGetById = async (request, h) => {
     };
 };
 
-const getAllTips = () => ({
-    status: 'success',
-    data: {
-        tips,
+const getAllTips = async (request, h) => {
+    const tipsSnapshot = await fs_tips.collection("tips").get();
+
+    if (tipsSnapshot.empty) {
+        const response = h.response({
+            status: "fail",
+            message: "No tips found",
+        });
+        response.code(404);
+        return response;
     }
-});
+
+    const tips = tipsSnapshot.docs.map(doc => doc.data());
+
+    return {
+        status: "success",
+        data: tips,
+    };
+};
 
 module.exports = { tipsGetById, getAllTips };
