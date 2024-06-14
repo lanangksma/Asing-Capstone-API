b0c16593a38ad5ab446d2d26c2e93ee00f89f036
lanangksma <kusumalanang1@gmail.com>
2024-06-14 19:28:07 +0700

    fix error

--- src/routes/foodRoutes.js
+++ src/routes/foodRoutes.js
@@ -1,21 +1,26 @@
 const foodsController = require("../controllers/foodsController");
 
 const foodsRoutes = [
-    {
-        method: "POST",
-        path: "/add",
-        handler: foodsController.addFood,
-    },
-    {
-        method: "GET",
-        path: "/food/{id}",
-        handler: foodsController.foodGetById,
-    },
-    {
-        method: "POST",
-        path: "/addmany",
-        handler: foodsController.addFoods,
-    },
+  {
+    method: "POST",
+    path: "/add",
+    handler: foodsController.addFood,
+  },
+  {
+    method: "GET",
+    path: "/food/id/{id}",
+    handler: foodsController.foodGetById,
+  },
+  {
+    method: "POST",
+    path: "/addmany",
+    handler: foodsController.addFoods,
+  },
+  {
+    method: "GET",
+    path: "/food",
+    handler: foodsController.foodGetByName,
+  },
 ];
 
 module.exports = foodsRoutes;
